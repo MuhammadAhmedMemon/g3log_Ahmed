4edaaf0c2b8777640f4c34bb99ef31750e923014
Muhammad Ahmed Memon <allomian@localhost.localdomain>
2020-05-29 00:26:59 +0500

    Update

--- main.cpp
+++ main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
+#include <thread>
 #include "g3log/g3log.hpp"
 #include "g3log/logworker.hpp"
 
-void runG3logger(){
-    const std::string directory = "../logs";
-    const std::string filename = "logfile";
+void runAhmedlogger(){
 
+    const std::string directory = "../logs";
+    const std::string filename = "AhmedLog"; 
     auto worker = g3::LogWorker::createLogWorker();
     auto sinkHandle = worker->addDefaultLogger(filename,directory);
-
+    
     g3::initializeLogging(worker.get());
-    LOG(DEBUG) << "This is debug message";
+    
+     for(int i = 0;i<50;i++){
+           LOG(DEBUG) << "This is debug message from ahmedlogger";
+    }
 }
+void runLogiciellogger(){
+    
+    const std::string directory = "../logs";
+    const std::string filename = "logicielLog"; 
+    auto worker2 = g3::LogWorker::createLogWorker();
+    auto sinkHandle = worker2->addDefaultLogger(filename,directory);
+    
+    g3::initializeLogging(worker2.get());
+    
+     for(int i = 0;i<50;i++){
+           LOG(DEBUG) << "This is debug message from ahmedlogger";
+    }
+}
+
 
 
 int main(){
     
-    runG3logger();
+    std::thread th1(runAhmedlogger);
+
+    std::thread th2(runLogiciellogger);
+
+    th1.join();
+    th2.join();
+    
+    std::cout <<"Both threads completed"<<std::endl;
     return 0;
 }
